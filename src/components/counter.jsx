afe9c88b884738e04ead49639b6c3e49829e04ba
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2019-02-22 00:57:11 -0500

    Add event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 
 class Counter extends Component {
 
     state = {
         count: 0,
-        imageURL: "https://picsum.photos/200"
+        tags: ["tag1", "tag2", "tag3", "tag4"]
     };
 
     styles = {
         fontSize: 18,
         fontWeight: 'bold'
     };
+
+    constructor() {
+        {/*have to call super*/}
+        super();
+        /*this.handleIncrement = this.handleIncrement.bind(this);*/
+    }
+
+    renderTags() {
+        if (this.state.tags.length === 0) {
+            return <p>There are no tags</p>;
+        } else {
+            return <ul>
+                {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>;
+        }
+    }
+
+    handleIncrement = () => {
+        /*inherit the this using arrow function*/
+        this.state.count++;
+        this.setState({ count: this.state.count++}); /*tells react to update state*/
+    }
+
     render() {
         return (
             <React.Fragment>
-
                 <span style ={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Add to Cart</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Add to Cart</button>
+                <ul>
+
+                    {this.state.tags.length === 0 && <p>There are no tags</p>}
+                    {this.renderTags()}
+                </ul>
             </React.Fragment>
         );
     }
@@ -27,6 +54,7 @@ class Counter extends Component {
         console.log(count);
         return count === 0 ? 'Zero' : count;
     }
+
 }
 
 export default Counter;
