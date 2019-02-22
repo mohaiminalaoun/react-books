086dfd865790a23d50174376e619c09ddbeecdb8
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2019-02-21 23:48:13 -0500

    Inital React Set Up commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+/**
+ * Created by Mohaimin on 2/20/2019.
+ */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDom.render(<Counter/>, document.getElementById("root"));
