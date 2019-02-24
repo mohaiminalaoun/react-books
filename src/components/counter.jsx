5c9b5b255515da150e3573a132c3f4d5e8160df1
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2019-02-24 00:20:20 -0500

    Add dynamic rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,48 +4,27 @@ import React, {Component} from 'react';
 class Counter extends Component {
 
     state = {
-        count: 0,
-        tags: ["tag1", "tag2", "tag3", "tag4"]
+        value: this.props &&this.props.value
     };
 
-    styles = {
-        fontSize: 18,
-        fontWeight: 'bold'
-    };
 
-    constructor() {
-        {/*have to call super*/}
-        super();
-        /*this.handleIncrement = this.handleIncrement.bind(this);*/
-    }
-
-    renderTags() {
-        if (this.state.tags.length === 0) {
-            return <p>There are no tags</p>;
-        } else {
-            return <ul>
-                {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>;
-        }
-    }
 
     handleIncrement = () => {
         /*inherit the this using arrow function*/
-        this.state.count++;
-        this.setState({ count: this.state.count++}); /*tells react to update state*/
+        this.setState({ count: this.state.value++}); /*tells react to update state*/
     }
 
-    render() {
-        return (
-            <React.Fragment>
-                <span style ={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Add to Cart</button>
-                <ul>
 
-                    {this.state.tags.length === 0 && <p>There are no tags</p>}
-                    {this.renderTags()}
-                </ul>
-            </React.Fragment>
+    render() {
+        console.log("in render");
+        console.log("props", this.props);
+        return (
+            <div>
+                {this.props.children}
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button onClick={this.handleIncrement}
+                        className="btn btn-secondary btn-sm">Add to Cart</button>
+            </div>
         );
     }
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter'
+import Counters from './components/counters'
 
 
 
-ReactDom.render(<Counter/>, document.getElementById("root"));
+ReactDom.render(<Counters/>, document.getElementById("root"));
